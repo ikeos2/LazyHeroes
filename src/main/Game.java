8948ee177ea76389bfbdb4beac9dec78a891ec4b
Alex! <ikeos2@gmail.com>
2019-08-26 21:51:20 -0500

    Changed hero -> warrior, making hero an interface

---
+++ src/main/Game.java
@@ -0,0 +1,46 @@
+package main;
+
+public class Game {
+
+	public final static boolean OUTPUT = true;
+	public final static boolean STATS = true;
+	
+	public static void main(String[] args) {
+		
+		Hero Good = new Hero("Dudley Do Right", 10, 5, 10);
+		Hero Villian = new Hero("Wrongly Not Correct", 7, 5, 20);
+		
+		Battle(Good, Villian);
+	}
+	
+	public static int Battle(Hero A, Hero B) {
+		
+		while(true) {			
+
+			if(STATS) { 
+				String a_block = A.getName() + ": " +  A.getHealth() + "/100";
+				String b_block = B.getName() + ": " +  B.getHealth() + "/100";
+				System.out.println(a_block + "\n" + b_block); 
+			}
+		
+			if (A.tick() < 0) {
+				if(OUTPUT) { System.out.println(A.getName() + " has defeated " + B.getName()); }
+				return 1; 
+			}
+			if (B.tick() < 0) { 
+				if(OUTPUT) { System.out.println(A.getName() + " has defeated " + B.getName()); }
+				return -1;
+			}
+			
+			if(A.turn()) {
+				if(OUTPUT) { System.out.println(A.getName() + " attacks " + B.getName()); }
+				B.take_damage(A.attack());
+			}
+			if(B.turn()) {
+				if(OUTPUT) { System.out.println(B.getName() + " attacks " + A.getName()); }
+				A.take_damage(B.attack());
+			}
+		}
+	}
+
+}
---
+++ src/main/Warrior.java
@@ -0,0 +1,53 @@
+package main;
+
+public class Warrior {
+	static final int speedMax = 100;
+	
+	private String name;
+	private int speed, attack, defense;
+	private int hp;
+	
+	int speedBuildUp;
+	
+	public Warrior(String name, int attack, int defense, int speed) {
+		this.speed = speed;
+		this.name = name;
+		this.attack = attack;
+		this.defense = defense;
+		this.hp = 100;
+		
+		speedBuildUp = 0;
+	}
+	
+	public String getName() { return name; }
+	
+	public int attack() {
+		return attack;
+	}
+	
+	public void take_damage(int damage) {
+		hp -= damage - defense;
+	}
+	
+	public int getHealth() {
+		return hp;
+	}
+	
+	public boolean turn() {
+		if (1.0 <= (speedBuildUp / speedMax)  ) {
+			speedBuildUp -= speedMax;
+			return true;
+		} else {
+			return false;
+		}
+		//return (1.0 <= (speedBuildUp / 100) ? true : false);
+	}
+	
+	public int tick() {
+		speedBuildUp += speed;
+		
+		if(hp <= 0) return -1;
+		
+		return 0;
+	}
+}
